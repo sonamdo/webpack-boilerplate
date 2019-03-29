7a34e6bbfeaba7c070c89c5e2acf64585d8f8f1a
sonamdo <sonamklein@gmail.com>
2019-03-29 14:36:50 -0400

    removed unnecessary files, fixed an issue with a dependancy

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import Header from './Header.js';
-import Banner from './Banner.js';
-import About from './About.js';
-import About2 from './About2.js';
-import Contact from './Contact.js'
-import Footer from './Footer.js'
 
 const Main = () => (
   <div>
-    <Header />
-    <Banner />
-    <About />
-    <About2 />
-    <Contact />
-    <Footer />
+    <h1>Boilerplate Up</h1>
   </div>
 )
 
